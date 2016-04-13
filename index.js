4e867c4e0dd4ce6fa746eee2f1fd06ff3a134cba
arunthampi <arun.thampi@gmail.com>
2016-04-12 19:08:13 -0700

    Working checkout power

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var path = require('path');
 
 module.exports = function(robot) {
-  robot.loadfile(path.resolve(__dirname, "src"), "checkout.js");
+  robot.loadFile(path.resolve(__dirname, "src"), "checkout.js");
 };
--- src/checkout.js
+++ src/checkout.js
@@ -1,17 +1,22 @@
 module.exports = function(robot) {
   var resourceMatch = /^resource-(.+)$/i;
 
-  robot.respond(/list resources/i, function(res, done) {
-    var data, key, resource, response, value;
-    data = robot.brain.data._private;
-    response = ", \nThe following resource(s) are checked out: \n";
+  robot.respond(/list resources?/i, function(res, done) {
+    var data = robot.brain.data._private;
+    var response = "";
+    var count = 0;
 
-    for (key in data) {
-      value = data[key];
-      resource = resourceMatch.exec(key);
+    for (var key in data) {
+      var value = data[key];
+      var resource = resourceMatch.exec(key);
       if (resource !== null) {
         response += "'" + resource[1] + "' was checked out by " + value + "\n";
+        count++;
       }
+    }
+
+    if(count == 0) {
+      response = "No resources have been checked out";
     }
 
     res.reply(response, done);
@@ -24,9 +29,9 @@ module.exports = function(robot) {
 
     if (resourceUser === null) {
       robot.brain.set('resource-' + resourceName, username);
-      res.reply(" checked out " + resourceName, done);
+      res.reply(" <@" + username + "> checked out " + resourceName, done);
     } else {
-      res.reply("Seems like " + resourceName + " is already checked out by " + resourceUser + ".", done);
+      res.reply("Seems like " + resourceName + " is already checked out by <@" + resourceUser + ">.", done);
     }
   });
 
@@ -36,12 +41,12 @@ module.exports = function(robot) {
     var resourceUser = robot.brain.get('resource-' + resourceName);
 
     if (resourceUser === null) {
-      res.reply(", " + resourceName + " does not appear to be checked out", done);
+      res.reply("'" + resourceName + "' does not appear to be checked out", done);
     } else if (resourceUser === username) {
       robot.brain.remove('resource-' + resourceName);
-      res.reply("returned " + resourceName, done);
+      res.reply("<@" + resourceUser + "> returned " + resourceName, done);
     } else {
-      res.reply(", " + resourceName + " is checked out by " + resourceUser + ".", done);
+      res.reply("'" + resourceName + "' is checked out by " + resourceUser + ".", done);
     }
   });
 
@@ -51,10 +56,10 @@ module.exports = function(robot) {
     var resourceUser = robot.brain.get('resource-' + resourceName);
 
     if (resourceUser === null) {
-      res.reply(", " + resourceName + " does not appear to be checked out", done);
+      res.reply("'" + resourceName + "' does not appear to be checked out", done);
     } else {
       robot.brain.remove('resource-' + resourceName);
-      res.reply("forcefully returned " + resourceName, done);
+      res.reply("<@" + username + "> forcefully returned " + resourceName, done);
     }
   });
 };
